f2dabbd430e62497b03b1bbc63913d21922faf8f
hrishikeshprateek <prateekhrishikesh@gmail.com>
2021-04-05 20:00:23 +0530

    init

--- app/src/main/java/com/thundersharp/bombaydine/user/core/Adapters/AllItemAdapter.java
+++ app/src/main/java/com/thundersharp/bombaydine/user/core/Adapters/AllItemAdapter.java
@@ -5,21 +5,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.thundersharp.bombaydine.R;
+import com.thundersharp.bombaydine.user.core.aligantnumber.ElegantNumberInteractor;
+import com.thundersharp.bombaydine.user.core.aligantnumber.ElegentNumberHelper;
 
 import java.util.HashMap;
 import java.util.List;
 
-public class AllItemAdapter extends RecyclerView.Adapter<AllItemAdapter.ViewHolder> {
+public class AllItemAdapter extends RecyclerView.Adapter<AllItemAdapter.ViewHolder>  implements ElegantNumberInteractor.setOnTextChangeListner {
 
     List<Object> itemObjectlist;
     Context context;
+    ElegentNumberHelper elegentNumberHelper;
 
     public AllItemAdapter(){}
 
@@ -31,12 +36,17 @@ public class AllItemAdapter extends RecyclerView.Adapter<AllItemAdapter.ViewHold
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.uneversal_holder,parent,false));
+        View view =  LayoutInflater.from(context).inflate(R.layout.uneversal_holder,parent,false);
+        elegentNumberHelper = new ElegentNumberHelper(context,this,view);
+        return new  ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         HashMap<String,String> hashMap = (HashMap<String,String>)itemObjectlist.get(position);
+
+        elegentNumberHelper.bindviewHolder(holder.initial,holder.finalview,R.id.minus,R.id.plus,R.id.displaytext,R.id.plusinit);
+        elegentNumberHelper.getcurrentnumber();
 
         holder.name.setText(hashMap.get("name"));
         Glide.with(context).load(hashMap.get("imageuri")).into(holder.imageView);
@@ -47,16 +57,26 @@ public class AllItemAdapter extends RecyclerView.Adapter<AllItemAdapter.ViewHold
         if (itemObjectlist != null) return itemObjectlist.size();else return 0;
     }
 
+    @Override
+    public int OnTextChangeListner(int val) {
+        //Toast.makeText(context,""+val,Toast.LENGTH_SHORT).show();
+        return 0;
+    }
+
     class ViewHolder extends RecyclerView.ViewHolder {
 
         ImageView imageView;
         TextView name;
+        LinearLayout initial,finalview;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             imageView = itemView.findViewById(R.id.imageview);
             name = itemView.findViewById(R.id.name);
+
+            initial = itemView.findViewById(R.id.initial);
+            finalview = itemView.findViewById(R.id.finl);
         }
     }
 }
---
+++ app/src/main/java/com/thundersharp/bombaydine/user/core/aligantnumber/ElegantNumberInteractor.java
@@ -0,0 +1,24 @@
+package com.thundersharp.bombaydine.user.core.aligantnumber;
+
+import android.widget.LinearLayout;
+
+public interface ElegantNumberInteractor {
+
+    interface ViewBinder {
+
+        void bindviewHolder(LinearLayout initialView, LinearLayout finalview, int plusbuttonId, int minusbuttonId,int textviewid,int plusinitial);
+
+    }
+
+    interface Presentor{
+
+        int getcurrentnumber();
+
+    }
+
+    interface setOnTextChangeListner{
+
+        int OnTextChangeListner(int val);
+    }
+
+}
