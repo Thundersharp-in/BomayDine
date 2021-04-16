90c1b0aed3f380c3e47110cafd23cceed28301a3
AmritaSingh09 <amritasingh09092002@gmail.com>
2021-04-16 06:17:49 +0530

    1st commit on 16-04-2021 by amrita

--- app/src/main/java/com/thundersharp/bombaydine/user/ui/account/Profile.java
+++ app/src/main/java/com/thundersharp/bombaydine/user/ui/account/Profile.java
@@ -7,16 +7,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.thundersharp.bombaydine.R;
 
 public class Profile extends Fragment {
 
+    TextView view_profile;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view= inflater.inflate(R.layout.fragment_profile, container, false);
+
+        view_profile=view.findViewById(R.id.view_profile);
+
+        view_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return  view;
+
     }
 }
