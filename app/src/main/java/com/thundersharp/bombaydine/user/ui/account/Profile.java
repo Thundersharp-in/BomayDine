f32ac911809a2eed780d3b457b1b05b8cd9fc11d
AmritaSingh09 <amritasingh09092002@gmail.com>
2021-04-26 19:00:48 +0530

    2st commit on 26-04-2021 by amrita

--- app/src/main/java/com/thundersharp/bombaydine/user/ui/account/Profile.java
+++ app/src/main/java/com/thundersharp/bombaydine/user/ui/account/Profile.java
@@ -1,8 +1,11 @@
 package com.thundersharp.bombaydine.user.ui.account;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,7 +28,6 @@ public class Profile extends Fragment {
         view_profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
             }
         });
 
