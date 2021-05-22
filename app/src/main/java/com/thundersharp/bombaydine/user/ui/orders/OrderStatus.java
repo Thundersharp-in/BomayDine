74c3c45c91f602a39fd634a162191bf17935bc7c
AmritaSingh09 <amritasingh09092002@gmail.com>
2021-05-22 16:23:18 +0530

    1st Commit by Amrita at May-22-2021

--- app/src/main/java/com/thundersharp/bombaydine/user/ui/orders/OrderStatus.java
+++ app/src/main/java/com/thundersharp/bombaydine/user/ui/orders/OrderStatus.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.vipulasri.timelineview.TimelineView;
@@ -27,6 +28,8 @@ public class OrderStatus extends AppCompatActivity {
     private OrederBasicDetails orederBasicDetails;
     private TimelineView timelineView;
 
+    private TextView fav,unfav;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +41,24 @@ public class OrderStatus extends AppCompatActivity {
             Toast.makeText(this, "Error in getting details", Toast.LENGTH_SHORT).show();
             finish();
         }
+
+        fav=findViewById(R.id.fav);
+        unfav=findViewById(R.id.unfav);
+
+        fav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fav.setVisibility(View.GONE);
+                unfav.setVisibility(View.VISIBLE);
+            }
+        });
+        unfav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                unfav.setVisibility(View.GONE);
+                fav.setVisibility(View.VISIBLE);
+            }
+        });
 
         timelineView = findViewById(R.id.timeline);
         TimeLineAdapter timeLineAdapter = new TimeLineAdapter(getdata(),Integer.parseInt(orederBasicDetails.getStatus()));
