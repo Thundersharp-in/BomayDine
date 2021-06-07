f2067c96dbb2470e03320ff00961633a28559816
AmritaSingh09 <amritasingh09092002@gmail.com>
2021-06-08 04:09:22 +0530

    Commited on 8 Jun by Amrita on admin

---
+++ Admin/src/main/java/com/thundersharp/admin/core/Adapters/RecentAdapter.java
@@ -0,0 +1,23 @@
+package com.thundersharp.admin.core.Adapters;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.thundersharp.admin.R;
+
+import java.util.List;
+
+public class RecentAdapter  {
+
+
+}
---
+++ Admin/src/main/java/com/thundersharp/admin/core/orders/OrderHistoryProvider.java
@@ -0,0 +1,108 @@
+package com.thundersharp.admin.core.orders;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.thundersharp.admin.core.utils.CONSTANTS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderHistoryProvider implements OrderContract{
+
+    static OrderHistoryProvider orderHistoryProvider;
+    static int renderNoq = 6;
+
+    public static OrderHistoryProvider getOrderInstance(int renderNo){
+        orderHistoryProvider = new OrderHistoryProvider();
+        renderNoq = renderNo;
+        return orderHistoryProvider;
+    }
+
+    public OrderHistoryProvider setOnOrderFetchListener(OrderContract.onOrderFetch onOrderFetchListener){
+        orderHistoryProvider.OrderHistoryProvider(onOrderFetchListener);
+        return orderHistoryProvider;
+    }
+    public OrderHistoryProvider(){}
+
+    public void OrderHistoryProvider(OrderContract.onOrderFetch onOrderFetch) {
+        this.onOrderFetch = onOrderFetch;
+    }
+
+    private OrderContract.onOrderFetch onOrderFetch;
+
+    @Override
+    public void fetchRecentOrders() {
+        List<Object> objects = new ArrayList<>();
+        int counter = 0;
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            if (renderNoq == 0){
+                FirebaseDatabase
+                        .getInstance()
+                        .getReference(CONSTANTS.DATABASE_NODE_ALL_USERS)
+                        .child(FirebaseAuth.getInstance().getUid())
+                        .child(CONSTANTS.DATABASE_NODE_ORDERS)
+                        .child(CONSTANTS.DATABASE_NODE_OVERVIEW)
+                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if (snapshot.exists()) {
+
+                                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                                        objects.add(dataSnapshot);
+                                    }
+
+                                    onOrderFetch.onOrderFetchSuccess(objects);
+
+                                } else {
+                                    Exception exception = new Exception("ERROR 404 : NO DATA FOUND");
+                                    onOrderFetch.onDataFetchFailure(exception);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                onOrderFetch.onDataFetchFailure(error.toException());
+                            }
+                        });
+
+            }else {
+                FirebaseDatabase
+                        .getInstance()
+                        .getReference(CONSTANTS.DATABASE_NODE_ALL_USERS)
+                        .child(FirebaseAuth.getInstance().getUid())
+                        .child(CONSTANTS.DATABASE_NODE_ORDERS)
+                        .child(CONSTANTS.DATABASE_NODE_OVERVIEW)
+                        .limitToFirst(renderNoq)
+                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if (snapshot.exists()) {
+
+                                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                                        objects.add(dataSnapshot);
+                                    }
+                                    onOrderFetch.onOrderFetchSuccess(objects);
+
+                                } else {
+                                    Exception exception = new Exception("ERROR 404 : NO DATA FOUND");
+                                    onOrderFetch.onDataFetchFailure(exception);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                onOrderFetch.onDataFetchFailure(error.toException());
+                            }
+                        });
+            }
+        }else {
+            Exception exception = new Exception("User not Logged in.");
+            onOrderFetch.onDataFetchFailure(exception);
+        }
+    }
+}
