1a107509c77d862c3cd76736fd12d0cea5778a34
hrishikeshprateek <prateekhrishikesh@gmail.com>
2021-06-13 08:58:35 +0530

    Updated Admin Core modules

--- Admin/src/main/java/com/thundersharp/admin/ui/edits/CategoryAddEdit.java
+++ Admin/src/main/java/com/thundersharp/admin/ui/edits/CategoryAddEdit.java
@@ -1,15 +1,22 @@
 package com.thundersharp.admin.ui.edits;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.material.textfield.TextInputLayout;
 import com.thundersharp.admin.R;
 import com.thundersharp.admin.core.Model.CategoryData;
 
 public class CategoryAddEdit extends AppCompatActivity {
 
     CategoryData categoryData;
+    ImageView imagehome;
+    AppCompatButton categoryUpdate;
+    TextInputLayout categoryName,url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,20 @@ public class CategoryAddEdit extends AppCompatActivity {
 
         }
 
+        imagehome = findViewById(R.id.imagehome);
+        categoryName = findViewById(R.id.cn);
+        url = findViewById(R.id.url);
+        categoryUpdate = findViewById(R.id.updatec);
+
+        if (getIntent().getSerializableExtra("data") !=null) {
+            Glide.with(this).load(categoryData.getIMAGES()).into(imagehome);
+            categoryName.getEditText().setText(categoryData.getNAME());
+            url.getEditText().setText(categoryData.getIMAGES());
+        }
+
+        categoryUpdate.setOnClickListener(v->{
+
+        });
 
     }
 }
